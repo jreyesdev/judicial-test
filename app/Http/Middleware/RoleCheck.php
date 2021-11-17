2f533b6aa162e376b083c06ac69e0139ab3add88
Jesus <jesus@gmail.com>
2021-11-17 15:17:17 -0400

    Middleware de Rol

---
+++ app/Http/Middleware/RoleCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next,$rol)
+    {
+        return ($request->user()->rol[0]->name  !== $rol) ? abort(403) : $next($request);
+    }
+}
