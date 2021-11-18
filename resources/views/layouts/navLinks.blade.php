a78c25e53da7f4a431567af80fffd718063aea1e
jreyesdev <djrm1204@gmail.com>
2021-11-18 00:45:47 -0400

    Ruta facturas

--- resources/views/layouts/navLinks.blade.php
+++ resources/views/layouts/navLinks.blade.php
@@ -3,5 +3,8 @@
         {{ __('Productos') }}
     </x-nav-link>
     @if (auth()->user()->rol[0]->name === 'administrador')
+    <x-nav-link :href="route('facturas')" :active="request()->routeIs('facturas')">
+        {{ __('Facturas') }}
+    </x-nav-link>
     @endif
 </div>
--- resources/views/layouts/respNavLinks.blade.php
+++ resources/views/layouts/respNavLinks.blade.php
@@ -2,4 +2,9 @@
     <x-responsive-nav-link :href="route('products')" :active="request()->routeIs('products')">
         {{ __('Productos') }}
     </x-responsive-nav-link>
+    @if (auth()->user()->rol[0]->name === 'administrador')
+    <x-responsive-nav-link :href="route('facturas')" :active="request()->routeIs('facturas')">
+        {{ __('Facturas') }}
+    </x-responsive-nav-link>
+    @endif
 </div>
