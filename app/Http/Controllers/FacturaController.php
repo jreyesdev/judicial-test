d42866ad65a67547855eb1c372994df415fe28cd
jreyesdev <djrm1204@gmail.com>
2021-11-18 16:10:54 -0400

    Metodo para generar factura. correccion otros metodos

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Compra;
 use App\Models\Factura;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class FacturaController extends Controller
@@ -16,7 +17,10 @@ class FacturaController extends Controller
     public function index()
     {
         $comp = Compra::count();
-        $facts = Factura::orderBy('created_at', 'desc')->paginate();
+        $facts = Factura::select(['codigo', 'user_id', 'created_at'])
+            ->addSelect(DB::raw('sum(cant * (price * (tax + 1))) as total'))
+            ->groupBy('codigo')->groupBy('user_id')->groupBy('created_at')
+            ->orderBy('created_at', 'desc')->paginate();
         return view('facturas.index', compact('facts', 'comp'));
     }
 
@@ -35,11 +39,64 @@ class FacturaController extends Controller
     }
 
     /**
+     * Genera las facturas
+     *
+     */
+    public function generar()
+    {
+        // Agrupa por cliente
+        $facts = Compra::select(['product_id', 'user_id'])
+            ->addSelect(DB::raw('count(product_id) as count_prod'))
+            ->groupBy('user_id')->groupBy('product_id')
+            ->get()->groupBy(function ($item) {
+                return $item->user_id;
+            });
+        // Inserta en Facturas
+        $facts->map(function ($items, $clientId) {
+            $cod = $this->generaCodigo();
+            $items->map(function ($prod) use ($cod) {
+                Factura::create([
+                    'codigo' => $cod,
+                    'nombre_producto' => $prod->product->name,
+                    'cant' => $prod->count_prod,
+                    'price' => $prod->product->price,
+                    'tax' => $prod->product->tax,
+                    'user_id' => $prod->user_id,
+                ]);
+            });
+            Compra::whereUserId($clientId)->delete();
+        });
+        return redirect(route('facturas'))
+            ->withAlert(['success', 'Facturas generadas']);
+        /*
+        [
+            'codigo',
+            'nombre_producto',
+            'cant',
+            'price',
+            'tax',
+            'user_id',
+        ]
+
+        $facts = Compra::toBase()->get()
+            ->groupBy(function ($item) {
+                return $item->user_id;
+            });
+        $facts = Compra::select(['product_id', 'user_id'])
+            ->addSelect(DB::raw('count(product_id) as count'))
+            ->groupBy('product_id')->groupBy('user_id')
+            ->get();
+        */
+    }
+
+    /**
      * Retorna codigo de la factura
      * @return string
      */
     protected function generaCodigo()
     {
-        return now()->format('ymd-') . (Factura::count() + 1);
+        $cant = Factura::select(['codigo'])->groupBy('codigo')
+            ->get()->count() + 1;
+        return now()->format('ymd-') . $cant;
     }
 }
